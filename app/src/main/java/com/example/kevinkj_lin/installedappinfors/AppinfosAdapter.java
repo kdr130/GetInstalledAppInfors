2ee1dee0882bb342ac597543d7d0963dc17762e7
KevinKJ Lin(林楷峻) <KevinKJ_Lin@corpnet.asus>
2016-11-02 10:57:32 +0800

    add recyclerview to fragment

---
+++ app/src/main/java/com/example/kevinkj_lin/installedappinfors/AppinfosAdapter.java
@@ -0,0 +1,59 @@
+package com.example.kevinkj_lin.installedappinfors;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AppinfosAdapter extends RecyclerView.Adapter<AppinfosAdapter.ViewHolder> {
+    // ViewHolder 為 item layout
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView appNameTextView;
+        public ImageView appIconTextView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            appNameTextView = (TextView) itemView.findViewById(R.id.tv_name);
+            appIconTextView = (ImageView) itemView.findViewById(R.id.iv_icon);
+        }
+    }
+
+    private List<AppInfo> mAppInfos;
+
+    public AppinfosAdapter(List<AppInfo> appInfos){
+        mAppInfos = appInfos;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAppInfos.size();
+    }
+
+    // 將 layout 轉成 ViewHolder
+    // 相比 listView，可以減少 findViewById 的次數
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        View contactView = LayoutInflater.from(context).inflate(R.layout.item_appinfo, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(contactView);
+
+        return viewHolder;
+    }
+
+    // 從 list 中取得 App 資料 並放到 viewHolder 的對應元件內
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        AppInfo appInfo = mAppInfos.get(position);
+        TextView nameTextView = viewHolder.appNameTextView;
+        ImageView iconImageView = viewHolder.appIconTextView;
+
+        nameTextView.setText(appInfo.getName());
+        iconImageView.setImageDrawable(appInfo.getIcon());
+    }
+}
--- app/src/main/java/com/example/kevinkj_lin/installedappinfors/RecyclerViewFragment.java
+++ app/src/main/java/com/example/kevinkj_lin/installedappinfors/RecyclerViewFragment.java
@@ -1,8 +1,12 @@
 package com.example.kevinkj_lin.installedappinfors;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,9 +24,15 @@ public class RecyclerViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        RecyclerView rvAppInfos = (RecyclerView) view.findViewById(R.id.recyclerView);
+        Activity parentActivity = getActivity();
 
+        AppinfosAdapter adapter = new AppinfosAdapter(AppInfo.generateSampleList(parentActivity.getPackageManager()));
+        rvAppInfos.setAdapter(adapter);
+        rvAppInfos.setLayoutManager(new LinearLayoutManager(parentActivity));
+        // 使用 support lib 內建給 RecyclerView 的項目間隔線
+        rvAppInfos.addItemDecoration(new DividerItemDecoration(parentActivity, DividerItemDecoration.VERTICAL));
         return view;
     }
 
