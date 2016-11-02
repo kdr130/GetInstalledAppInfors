2a3519695fa350876e62768e044e25ed49c65144
KevinKJ Lin(林楷峻) <KevinKJ_Lin@corpnet.asus>
2016-11-02 09:56:03 +0800

    add new blank fragment to MainActivity

--- app/src/main/java/com/example/kevinkj_lin/installedappinfors/MainActivity.java
+++ app/src/main/java/com/example/kevinkj_lin/installedappinfors/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kevinkj_lin.installedappinfors;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFragment();
     }
+
+    private  void initFragment(){
+        RecyclerViewFragment rvFragment = new RecyclerViewFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.frameLayout, rvFragment);
+        ft.commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/kevinkj_lin/installedappinfors/RecyclerViewFragment.java
@@ -0,0 +1,28 @@
+package com.example.kevinkj_lin.installedappinfors;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RecyclerViewFragment extends Fragment {
+
+
+    public RecyclerViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+
+        return view;
+    }
+
+}
